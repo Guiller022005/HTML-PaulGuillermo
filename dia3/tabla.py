85976bf445cae2338aaf512d8c09efc258e3d8b5
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-04-08 07:48:41 -0500

    feat: :construction: tabla html
    Estructura básica de lo que contendrá la tabla HTML con ciertos estilos CSS aplicados. La tabla tendrá una fila de encabezado (<thead>) y un cuerpo de tabla (<tbody>) donde se mostrarán los datos.

--- dia3/tabla.py
+++ dia3/tabla.py
@@ -3,3 +3,50 @@ import json
 with open('dia3/tablas.json', 'r') as f:
     data = json.load(f)
     
+html_Table = """
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Tabla JSON a HTML</title>
+<style>
+    table {
+    width: 100%;
+    border.collapse: collapse;
+    }
+    th, td{
+      border: 1px solid #dddddd;
+      padding: 8px;
+      text-align: left;
+    }
+    th{
+      background-color: #f2f2f2;
+    }
+</style>
+</head>
+<body>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Index</th>
+            <th>GUID</th>
+            <th>Balance</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>About</th>
+            <th>Registered</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Tags</th>
+            <th>Friends</th>
+        </tr>
+    </thead>
+    <tbody>
+"""
+
